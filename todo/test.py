a2dfbc9d1819f431d4793a2bea5ddd5e0f69aef8
Kalys01 <kalysosmon96.g@gmail.com>
2021-01-09 20:00:47 +0600

    my todoList in python

---
+++ todo/test.py
@@ -0,0 +1,15 @@
+test_str = "Pythonisssst"
+ 
+#count = sum(map(lambda x : 1 if 's' in x else 0, test_str)) 
+  
+cont = sum(map(lambda x: 1 if 's' in x else 0, test_str))
+print (cont)
+
+s = "Madina kalys, ka"
+def sp(x):
+    return [y for y in x]
+
+print(list(s))
+
+print(s.count('a'))
+print(dir(234))
