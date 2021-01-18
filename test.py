6f42574989d2511e255039d58245deaf4fc724be
Kalys01 <kalysosmon96.g@gmail.com>
2021-01-18 17:02:40 +0600

    created superuser and datas

---
+++ test.py
@@ -0,0 +1,28 @@
+# a = 7
+# b = 7
+# while False:
+#     a = 3
+#     b = 4
+
+# print(b - a)
+
+# def pr(x):
+#     for i in range(x):
+#         print(i)
+#         return
+
+# pr(10)
+
+# arr = [4, 3, 1, 2]
+# arr[0], arr[arr[0] - 1] = arr[arr[0] -1], arr[0]
+# print(arr) # [2, 4, 1, 2]
+
+# a = 16 % 20 // 2**2
+# lst = [1,1,2,3,5,8,13]
+# print(lst[lst[a]] * len(lst)) # 56
+
+class Test:
+    _eggs = 7
+
+t = Test()
+print(t._eggs)
